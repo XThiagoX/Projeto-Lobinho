051d5cc05c75088c1b7a50610f84648a97cd0fe7
Thiago Araujo <thiago_a@id.uff.br>
2022-07-31 20:35:38 -0300

    feat(cadastros de lobinho): Requisção de cadastra realizada

---
+++ Adicionar Lobinho/addWolfeScript.js
@@ -0,0 +1,36 @@
+const urlApi = "https://lobinhos.herokuapp.com/wolves"
+
+async function postWolfInformation(wolfeName, wolfeAge, wolfeImage, wolfeDescription){
+
+    // let wolfeName = document.querySelector(".inputWolfeName").value
+    // let wolfeAge = document.querySelector(".inputWolfeAge").value
+    // let wolfeImage = document.querySelector(".inputlinkPhoto").value
+    // let wolfeDescription = document.querySelector(".textareaDescription").value
+
+    let fetchBody = {
+        name: wolfeName,
+        age : wolfeAge,
+        image_url : wolfeImage,
+        description : wolfeDescription        
+    }
+
+    let fetchConfig = {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(fetchBody)
+    }
+
+    await fetch(urlApi, fetchConfig)
+        .then( answer =>  answer.json() 
+            .then(resp => { alert("SUCESSO! Seu lobo foi cadastrado.") })
+            .catch(error => { alert("ERRO! Houve algum problema, tente novamente mais tarde.") }))
+        .catch(error => { console.log(error) }) 
+}
+
+let wolfeName = document.querySelector(".inputWolfeName").value
+let wolfeAge = document.querySelector(".inputWolfeAge").value
+let wolfeImage = document.querySelector(".inputlinkPhoto").value
+let wolfeDescription = document.querySelector(".textareaDescription").value
+
+let buttonSave = document.querySelector(".buttonSave")
+buttonSave.addEventListener('click', e => postWolfInformation(wolfeName, wolfeAge, wolfeImage, wolfeDescription))
