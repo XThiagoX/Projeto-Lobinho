ded0f6c9b4a394b0d7064ac7f7166a582ba4706d
Thiago Araujo <thiago_a@id.uff.br>
2022-07-31 21:50:57 -0300

    refactor(identação):

--- Adicionar Lobinho/addWolfeScript.js
+++ Adicionar Lobinho/addWolfeScript.js
@@ -2,11 +2,6 @@ const urlApi = "https://lobinhos.herokuapp.com/wolves"
 
 async function postWolfInformation(wolfeName, wolfeAge, wolfeImage, wolfeDescription){
 
-    // let wolfeName = document.querySelector(".inputWolfeName").value
-    // let wolfeAge = document.querySelector(".inputWolfeAge").value
-    // let wolfeImage = document.querySelector(".inputlinkPhoto").value
-    // let wolfeDescription = document.querySelector(".textareaDescription").value
-
     let fetchBody = {
         name: wolfeName,
         age : wolfeAge,
@@ -23,7 +18,8 @@ async function postWolfInformation(wolfeName, wolfeAge, wolfeImage, wolfeDescrip
     await fetch(urlApi, fetchConfig)
         .then( answer =>  answer.json() 
             .then(resp => { alert("SUCESSO! Seu lobo foi cadastrado.") })
-            .catch(error => { alert("ERRO! Houve algum problema, tente novamente mais tarde.") }))
+            .catch(error => { alert("ERRO! Houve algum problema, tente novamente mais tarde.", error ) 
+                console.log(error) }))
         .catch(error => { console.log(error) }) 
 }
 
