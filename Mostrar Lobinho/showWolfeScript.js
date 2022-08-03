e4a192f28bc75151a9a116f3b8d54258aa35e604
Thiago Araujo <thiago_a@id.uff.br>
2022-08-03 00:17:40 -0300

    docs(organização e e comentários de código):

--- Mostrar Lobinho/showWolfeScript.js
+++ Mostrar Lobinho/showWolfeScript.js
@@ -13,22 +13,20 @@ buttonDelete.textContent = "EXCLUIR"
 
 function showWolfe(wolfe){
     // Criação e obtenção de elemtos da página para conter as informações
-    let main = document.querySelector('#mainShowWolfe')
-    // let main = document.createElement('main')
-    // main.setAttribute('id', 'mainShowWolfe')
+    let main = document.querySelector('#mainShowWolfe')    
 
     let divWolfeName = document.createElement('div')
     divWolfeName.setAttribute('class', 'WolfeName')
     
     let wolfeName = document.createElement('h1')
     wolfeName.innerHTML = wolfe.name        
-    // let card = document.querySelector('.card') 
+
     let card = document.createElement('div')
     card.setAttribute('class', 'card')
     
     let container = document.createElement('div')
     container.setAttribute('class','ContainerImgButtons')
-    // let container = document.querySelector('.ContainerImgButtons')    
+   
     let imageWolfe = document.createElement('img')
     imageWolfe.setAttribute('class', 'wolfe')
     imageWolfe.setAttribute('src', wolfe.image_url)         
@@ -36,12 +34,6 @@ function showWolfe(wolfe){
     let divButtons = document.createElement('div')
     divButtons.setAttribute('class', 'buttons')
 
-    // let buttonAdopt = document.createElement('button')
-    // buttonAdopt.setAttribute('class', 'buttonAdopt')
-    // buttonAdopt.textContent = "ADOTAR"
-    // let buttonDelete = document.createElement('button')
-    // buttonDelete.setAttribute('class', 'buttonDelete')
-    // buttonDelete.textContent = "EXCLUIR"
 
     let divDescription = document.createElement('div')
     divDescription.setAttribute('class', 'descriptionText')
